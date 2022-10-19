e4e175712d38b4de74abb1886ff893a0d830be50
kostin <kostin_vadim@icloud.com>
2022-10-19 20:28:37 +0300

    step: deploy 1.0.1

--- source/core/views.py
+++ source/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from core.serializers import CreateUserSerializer
+
+
+class SignupView(generics.CreateAPIView):
+    serializer_class = CreateUserSerializer
+
