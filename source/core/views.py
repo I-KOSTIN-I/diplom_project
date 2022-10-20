2ff9f3803d172a6006fa44ee1aa066c3f1d33fd6
kostin <kostin_vadim@icloud.com>
2022-10-21 02:55:45 +0300

    step: deploy 1.0.1 added step 35.2

--- source/core/views.py
+++ source/core/views.py
@@ -1,8 +1,19 @@
-from rest_framework import generics
+from django.contrib.auth import login
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-from core.serializers import CreateUserSerializer
+from core.serializers import CreateUserSerializer, LoginSerializer
 
 
 class SignupView(generics.CreateAPIView):
     serializer_class = CreateUserSerializer
 
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        login(request=self.request, user=serializer.save())
+        return Response(serializer.data, status=status.HTTP_200_OK)
