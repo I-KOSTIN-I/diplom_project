cc4667c48bb543e4195e2433febb61d379436bab
kostin <kostin_vadim@icloud.com>
2022-10-21 03:19:10 +0300

    step: deploy 1.0.1 added step 35.4

--- source/core/views.py
+++ source/core/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import login
-from rest_framework import generics, status
+from django.contrib.auth import login, logout
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 
-from core.serializers import CreateUserSerializer, LoginSerializer
+from core.models import User
+from core.serializers import CreateUserSerializer, LoginSerializer, ProfileSerializer
 
 
 class SignupView(generics.CreateAPIView):
@@ -17,3 +18,16 @@ class LoginView(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         login(request=self.request, user=serializer.save())
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
