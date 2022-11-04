b45a7a5d4c85f3d65910a16890d4069bd07b49ed
kostin <kostin_vadim@icloud.com>
2022-11-05 01:38:43 +0300

    step: deploy 1.0.3 added step 37.1 created boards models

--- source/goals/models.py
+++ source/goals/models.py
@@ -29,21 +29,9 @@ class BoardParticipant(BaseModel):
         writer = 2, "Редактор"
         reader = 3, "Читатель"
 
-    board = models.ForeignKey(
-        Board,
-        verbose_name="Доска",
-        on_delete=models.PROTECT,
-        related_name="participants",
-    )
-    user = models.ForeignKey(
-        User,
-        verbose_name="Пользователь",
-        on_delete=models.PROTECT,
-        related_name="participants",
-    )
-    role = models.PositiveSmallIntegerField(
-        verbose_name="Роль", choices=Role.choices, default=Role.owner
-    )
+    board = models.ForeignKey(Board, verbose_name="Доска", on_delete=models.PROTECT, related_name="participants")
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.PROTECT, related_name="participants")
+    role = models.PositiveSmallIntegerField(verbose_name="Роль", choices=Role.choices, default=Role.owner)
 
     class Meta:
         unique_together = ("board", "user")
@@ -87,7 +75,7 @@ class Goal(BaseModel):
         to=GoalCategory,
         verbose_name='Категория',
         on_delete=models.CASCADE,
-        related_name='goals',
+        related_name='goals'
     )
     title = models.CharField(verbose_name='Название', max_length=255)
     description = models.TextField(verbose_name='Описание', null=True, blank=True)
