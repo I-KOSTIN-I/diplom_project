32fb5e106da27ff2050bbd225bf28dd76567103d
kostin <kostin_vadim@icloud.com>
2022-11-06 02:33:41 +0300

    step: deploy 1.0.3 added step 38.6 verification user

--- source/bot/models.py
+++ source/bot/models.py
@@ -5,7 +5,7 @@ from core.models import User
 
 class TgUser(models.Model):
     chat_id = models.BigIntegerField(verbose_name='Chat ID', unique=True)
-    user_ud = models.CharField(verbose_name='User UD', max_length=255, null=True, blank=True, default=None)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
+    username = models.CharField(verbose_name='Username', max_length=255, null=True, blank=True, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
     verification_code = models.CharField(max_length=32, null=True, blank=True, default=None)
 
